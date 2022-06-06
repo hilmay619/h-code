2c8b2701a9ecaa60a10361eac78b84c50b4594ca
Lucifer <hilmaydoank.45@gmail.com>
2022-06-06 22:23:56 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -44,29 +44,31 @@ def stats(update, context):
     mem_t = get_readable_file_size(memory.total)
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
-    stats = f'<b>Commit Date:</b> {last_commit}\n\n'\
-            f'<b>Bot Uptime:</b> {currentTime}\n'\
-            f'<b>OS Uptime:</b> {osUptime}\n\n'\
-            f'<b>Total Disk Space:</b> {total}\n'\
-            f'<b>Used:</b> {used} | <b>Free:</b> {free}\n\n'\
-            f'<b>Upload:</b> {sent}\n'\
-            f'<b>Download:</b> {recv}\n\n'\
-            f'<b>CPU:</b> {cpuUsage}%\n'\
-            f'<b>RAM:</b> {mem_p}%\n'\
-            f'<b>DISK:</b> {disk}%\n\n'\
-            f'<b>Physical Cores:</b> {p_core}\n'\
-            f'<b>Total Cores:</b> {t_core}\n\n'\
-            f'<b>SWAP:</b> {swap_t} | <b>Used:</b> {swap_p}%\n'\
-            f'<b>Memory Total:</b> {mem_t}\n'\
-            f'<b>Memory Free:</b> {mem_a}\n'\
-            f'<b>Memory Used:</b> {mem_u}\n'
+    stats = f'<b>⏲ Time Calculation ⏲</b>\n\n'\
+			f'<b>📅 Created : {last_commit}</b>\n'\
+            f'<b>⏰ Uptime : {currentTime}</b>\n'\
+            f'<b>🖥 OS Uptime : {osUptime}</b>\n\n'\
+			f'<b>📊 Data Usage 📊</b>\n\n'\
+            f'<b>💨 Total Disk Space : {total}</b>\n'\
+            f'<b>📈 Used : {used}</b> | <b> 📉 Free : {free}</b>\n'\
+            f'<b>🔺 Upload: {sent}</b>\n'\
+            f'<b>🔻 Download: {recv}</b>\n\n'\
+			f'<b>📊 Performance Meter 📊</b>\n\n'\
+            f'<b>🖥 CPU: {cpuUsage}%</b>\n'\
+            f'<b>⚙️ RAM: {mem_p}%</b>\n'\
+            f'<b>🪅 DISK: {disk}%</b>\n'\
+            f'<b>🎙 Physical Cores: {p_core}</b>\n'\
+            f'<b>🎙 Total Cores: {t_core}</b>\n'\
+            f'<b>🧬 Swap : {swap_t}</b> | <b>Used : {swap_p}%</b>\n'\
+            f'<b>⚙️ Memory Total : {mem_t}</b>\n'\
+            f'<b>⚙️ Memory Free : {mem_a}</b>\n'\
+            f'<b>⚙️ Memory Used : {mem_u}</b>\n'
     sendMessage(stats, context.bot, update.message)
 
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("My Master", "https://t.me/hilmay619")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
